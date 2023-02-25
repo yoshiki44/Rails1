7e27121f4224389a58d63efa87658aafdff2d747
阪本佳樹 <sakamotoyoshiki@YS-MacBook-Pro.local>
2023-02-25 23:39:22 +0900

    register

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Myapp3
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+
+    # デフォルトのロケールを:en以外に変更する
+    config.i18n.default_locale = :ja
+
+    # I18nライブラリに訳文の探索場所を指示する
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
